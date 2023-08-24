4561cd1effefdbfc12b418c2ab4a9b7c75fd53fb
gustavo-hellyan <gusthellyan@gmail.com>
2023-08-23 21:06:03 -0300

    Initial commit

---
+++ codigos_js/1 - helloworld.js
@@ -0,0 +1,9 @@
+window.alert("Hello, world!")
+window.confirm("Está gostando de JS?")
+
+var nome = window.prompt("Qual o seu nome?")    //nome recebe string digitada pelo usuário
+var idade = window.prompt("Qual a sua idade?")
+window.alert("Olá, "+nome+"! Você tem "+idade+" anos.")
+
+
+//Esse "window" pode ser omitido
